e2c2bc1d236c808e49b995ccf72e50c54353a89c
Moazz <moazzemh222@gmail.com>
2022-12-20 00:06:59 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    class Animal
+    {
+        public string name;
+        public int age;
+        public string color;
+        public void PrintDetails()
+        {
+            Console.WriteLine("Name: " + name + ", " + "Age: " + age + " Color: " + color);
+        }
+    }
+
+    class Dog: Animal
+    {
+        public string bred;
+        public void Sound()
+        {
+            Console.WriteLine("Dog sound: Ghew Ghew");
+        }
+        public Dog(string _name, int _age, string _color, string _bred)
+        {
+            name = _name;
+            age = _age;
+            color = _color;
+            bred = _bred;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Audi", 5, "Grey", "Siberian Huskey");
+            dog.PrintDetails();
+            Console.WriteLine(dog.bred);
+            dog.Sound();
+            Console.ReadLine();
+        }
+    }
+}
